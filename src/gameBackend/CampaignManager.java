833f76f2c0603cbb3a432c9998fba82d78d712ff
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-03 18:36:02 +0300

    Campaign service and campaign manager updated.

--- src/gameBackend/CampaignManager.java
+++ src/gameBackend/CampaignManager.java
@@ -1,5 +1,21 @@
 package gameBackend;
 
-public class CampaignManager {
+public class CampaignManager implements CampaignService {
 
+	@Override
+	public void add(Campaign campaign,Game game) {
+		System.out.println("Kampanya eklendi.");
+		System.out.println(game.getName() + " isimli oyun " + campaign.getCampaignDays() + " gunlugune %" + campaign.getDiscountRate()+ " oraninda indirime girmistir." );
+	}
+
+	@Override
+	public void update(Campaign campaign,Game game) {
+		System.out.println(game.getName() + " isimli oyunda " + campaign.getCampaignDays() + " gun gecerli kampanya guncellendi.");
+	}
+
+	@Override
+	public void delete(Campaign campaign,Game game) {
+		System.out.println(game.getName() + " isimli oyunda " + campaign.getCampaignDays() + " gun gecerli kampanya sona erdi.");
+	}
+	
 }
--- src/gameBackend/CampaignService.java
+++ src/gameBackend/CampaignService.java
@@ -1,5 +1,7 @@
 package gameBackend;
 
 public interface CampaignService {
-
+	void add(Campaign campaign, Game game);
+	void update(Campaign campaign,Game game);
+	void delete(Campaign campaign,Game game);
 }
