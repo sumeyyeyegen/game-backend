08fcd00a017fbe3db809b64d602b1544cedcf3ab
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-03 18:51:24 +0300

    Game related functions were created.

--- src/gameBackend/GameManager.java
+++ src/gameBackend/GameManager.java
@@ -1,5 +1,19 @@
 package gameBackend;
 
-public class GameManager {
+public class GameManager implements GameService {
+	@Override
+	public void add(Game game) {
+		System.out.println(game.getName() + " isimli oyun sisteme eklendi.");
+	}
 
+	@Override
+	public void update(Game game) {
+		System.out.println(game.getName() + " isimli oyun bilgileri guncellendi.");
+	}
+
+	@Override
+	public void delete(Game game) {
+		System.out.println(game.getName() + " isimli oyun sistemden silindi.");
+	}
+	
 }
--- src/gameBackend/GameService.java
+++ src/gameBackend/GameService.java
@@ -1,5 +1,7 @@
 package gameBackend;
 
 public interface GameService {
-	
+	void add(Game game);
+	void update(Game game);
+	void  delete(Game game);
 }
