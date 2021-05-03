1d34347f3b5f7f0170260853965c26fa35de46a9
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-03 17:53:29 +0300

    User service interface changed.

--- src/gameBackend/UserService.java
+++ src/gameBackend/UserService.java
@@ -2,7 +2,7 @@ package gameBackend;
 
 public interface UserService {
 	void register(User user);
-	void login(User user);
 	void update(User user);
 	void delete(User user);
+	void login(User user,String firstName,String lastName, String identityNumber,String birthYear);
 }
